6f29004f9d1634d05a239a6609ce0e9b758a87e1
rdosreis <rodrigocpdosreis@gmail.com>
2022-01-19 15:14:17 -0300

    aula 00

---
+++ RScripts/00_apresenta_curso.R
@@ -0,0 +1,41 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', out.height='80%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'hi_my_name_is.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----------
+knitr::include_graphics(here::here('images','videoaula-2.png'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='10%', paged.print=FALSE----------
+knitr::include_graphics(here::here('images','Rlogo.png'))
+
+
+## ---- echo=FALSE, eval=TRUE------------------------------------------------------------------------------------
+x <- rnorm(n = 100, mean = 10, sd = 1)
+
+
+## ---- echo=TRUE, eval=TRUE, fig.align='center', out.width='50%'------------------------------------------------
+hist(x, col = 'black', border = 'white')
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', paged.print=FALSE----------
+knitr::include_graphics(here::here('images','bugs_stan_logo.png'))
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', paged.print=FALSE-----------
+knitr::include_graphics(here('images','ctanlion.png'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', out.height='80%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'DoingBayesianDataAnalysis.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'Statistically-Insignificant-8.jpg'))
+
+
---
+++ RScripts/render_beamer.R
@@ -0,0 +1,33 @@
+library(here)
+library(rmarkdown)
+
+pasta <- "Rmds"
+arquivo <- "00_apresenta_curso"
+
+arquivo_rmd <- paste0(arquivo, ".Rmd")
+arquivo_pdf <- paste0(arquivo, ".pdf")
+input <- here::here(pasta, arquivo_rmd)
+arquivo_r <- paste0(arquivo, ".R")
+
+rmarkdown::render(input = input, 
+                  output_format = beamer_presentation(theme = "Antibes",
+                                                      highlight = "default",
+                                                      colortheme = "dolphin",
+                                                      fonttheme = "structurebold",
+                                                      includes = list(in_header = here::here("styles", "mystyle.tex")),
+                                                      slide_level = 2,
+                                                      keep_tex = FALSE,
+                                                      number_sections = FALSE,
+                                                      fig_caption = FALSE),
+                  output_file = arquivo_pdf,
+                  output_dir = here::here("output_pdf"),
+                  encoding = "UTF-8")
+
+knitr::purl(input = input,
+            output = here::here("RScripts", arquivo_r),
+            documentation = 1,
+            encoding = "UTF-8")
+
+
+# rmarkdown::render(input = input, 
+#                   output_format = "powerpoint_presentation")
